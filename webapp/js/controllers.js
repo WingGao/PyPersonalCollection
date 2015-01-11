6655789238f0eba1431d830797f4b966856ea364
Wing Gao <wing.gao@live.com>
2015-01-11 16:12:25 +0800

    modify login redirect

--- webapp/js/controllers.js
+++ webapp/js/controllers.js
@@ -131,7 +131,7 @@ angular.module('myApp.controllers', [])
     .controller('LoginCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
         $http.get('/user/check').success(function (res) {
             if (res.is_login) {
-                $location.path('/item/all/all/newest/0')
+                $location.path('/item/all/all/newest/1')
             } else {
                 window.location.href = '/user/login?next=/webapp'
             }
